8209b89665a85d24efdb2dcbf80c7f84a958e326
kiran-dhele <kkdhele777@gmail.com>
2021-12-10 12:01:44 +0530

    first commit

---
+++ src/test/java/com/neosoft/poc/main/ServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neosoft.poc.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
